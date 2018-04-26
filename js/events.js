e0a30bfdcea1193fc50f29e07547ecec3e313475
Newton Migosi <newtonmigosi@gmail.com>
2018-04-26 12:07:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,12 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function() {alert('Hey!'); return; });
+}
+
+function frameIt(){
+  $('img').on('load', function() {this.setAttribute} )
+}
+
 
 $(document).ready(function(){
 
