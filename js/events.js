a8140b75dc99836ce2f4e3f00c1c8ac7054a9354
Newton Migosi <newtonmigosi@gmail.com>
2018-04-26 12:19:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function() { 
+    alert('Hey!');
+    return;
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+    return;
+  });
+}
+
+function pressIt(){
+  $('input[type="text"]').on('keydown', function(key) {
+    if (key.which === 71) { alert("You pressed G!")}
+    return;
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  });  
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  // call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
